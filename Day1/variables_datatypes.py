98d2cebfdbc2127f2eca6b8c3f31daed518fb32d
Ramaneswari <chintalaramaneswari109@gmail.com>
2026-02-21 12:42:40 +0530

    Added Day1 variables practice program

---
+++ Day1/variables_datatypes.py
@@ -0,0 +1,39 @@
+a=10
+name="Rama"
+print(a)
+print(type(name))
+b=1.34
+print(type(b))
+stu=True
+print(stu)
+print(type(stu))
+
+
+#Type casting
+x="10"
+y=int(x)
+print(y)
+print(type(y))
+
+#interger to string
+num=50
+text=str(num)
+print(text)
+print(type(text))
+
+#basic math
+
+r=2
+s=4
+print(r+s)
+print(r-s)
+print(r*s)
+print(r//s)
+print(r**s)
+print(r%s)
+
+#example
+
+me="Ramaneswari"
+age=19
+print("My name is",me,"and I am",age,"year old.")
